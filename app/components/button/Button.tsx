0031434676cd5b74586d8db3a633621f424b3f3d
ariel920419 <118871692+ariel920419@users.noreply.github.com>
2025-04-04 16:09:07 +0800

    refactor: 0404-1

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 
 interface ButtonProps {
   label: string; // 按鈕上顯示的文字
@@ -24,23 +24,7 @@ function Button({ label }: ButtonProps) {
       >
         {label}
       </button>
-      <button
-        className="
-      bg-emerald-300
-      font-semibold
-      text-2xl
-      text-black
-      px-[1.5em]
-      py-[0.5em]
-      rounded-full
-      hover:bg-gray-800
-      hover:text-white
-      transition
-      duration-200
-    "
-      >
-        53123123213
-      </button>
+      
     </>
   );
 }

922c4b2fe2647f7217e9a0bb65fffeb27dee9da0
Max Lin <max60213@gmail.com>
2025-04-22 18:42:21 +0800

    remove: audio play button

--- app/species/SpeciesDetailModal.tsx
+++ app/species/SpeciesDetailModal.tsx
@@ -76,14 +76,6 @@ const SpeciesDetailModal = ({
           
           {/* 內容卡片 */}
           <div className="relative bg-[#4B4B50]/90 rounded-t-[40px] rounded-b-[30px] px-8 pt-28 pb-12">
-            {/* 播放按鈕 */}
-            <div className="absolute right-8 top-16 z-20">
-              <button className="w-12 h-12 flex items-center justify-center bg-[#44C2A5] rounded-full shadow-lg">
-                <svg className="ml-1" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M8 5.5L17.5 12L8 18.5V5.5Z" fill="white"/>
-                </svg>
-              </button>
-            </div>
             
             {/* 關閉按鈕 */}
             <div className="absolute top-6 right-6 z-30">
